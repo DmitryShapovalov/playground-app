da38802b99d50e9a02d73d5583e6d87f76bd746f
Dmytro Shapovalov <dshtorm@gmail.com>
2018-12-28 20:22:07 +0200

    adding playground page for additional screen

---
+++ Playground.playground/Pages/AdditionalScreen.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import PlaygroundSupport
+import Foundation
+import UIKit
+
+import App
+import UI
+
+let vc = AdditionalViewController()
+
+vc.assembleInteractions()
+
+PlaygroundPage.current.liveView = vc
